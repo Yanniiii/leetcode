961ceaac7c35d10f06b711bfa7ce03e8bc7ba44f
FengHuangQing <851117557@qq.com>
2021-01-21 19:41:53 +0800

    11

---
+++ leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by FengHuangQing on 2021/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
